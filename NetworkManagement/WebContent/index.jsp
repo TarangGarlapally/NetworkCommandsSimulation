3769dd7cf487661685931cec1856dec4c1c590d4
Tarang Garlapally <taranggarlapally333@users.noreply.github.com>
2021-01-03 16:11:51 +0530

    backend done withut options

--- NetworkManagement/WebContent/index.jsp
+++ NetworkManagement/WebContent/index.jsp
@@ -8,12 +8,45 @@
 </head>
 <body>
 
-<p>Hey Tarang</p>
+<p style="white-space: break-spaces;">Try this out!</p>
 <form action = "PingCmd" method = "get">
-<input type="text" name="pingurl" placeholder="Enter URL to ping"></input>
-<button type = "submit" name = "command" value="ping">Ping</button>
+	<input type="text" name="pingurl" placeholder="Enter URL to ping"></input>
+	<button type = "submit" name = "command" value="ping">ping</button>
+	<br /><br />
+	
+	<input type="text" name="tracerturl" placeholder="Enter URL to trace route"></input>
+	<button type = "submit" name = "command" value="tracert">tracert</button>
+	<br /><br />
+	
+	<input type="text" name="nslookupurl" placeholder="Enter URL to lookup"></input>
+	<button type = "submit" name = "command" value="nslookup">nslookup</button>
+	<br /><br />
+	
+	<input type="text" name="pathpingurl" placeholder="Enter URL to ping the path"></input>
+	<button type = "submit" name = "command" value="pathping">pathping</button>
+	<br /><br />
+	
+	<button type = "submit" name = "command" value="ipconfig">ipconfig</button>
+	<br /><br />
+	
+	<button type = "submit" name = "command" value="getmac">getmac</button>
+	<br /><br />
+	
+	<button type = "submit" name = "command" value="netstat">netstat</button>
+	<br /><br />
 </form>
+<%
+String output = request.getParameter("done");
+if(output != null){
+	for(String line: output.split("\n")){
+		%>
+		<p><%=line%></p>
+		<%
+	}
+}
 
-<p><%=request.getParameter("done") %></p>
+%>
+
+
 </body>
 </html>
--- NetworkManagement/src/PingCmd.java
+++ NetworkManagement/src/PingCmd.java
@@ -3,6 +3,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,21 +20,37 @@ public class PingCmd extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.print(request.getParameter("command"));
-		Process process = Runtime.getRuntime().exec("ping "+request.getParameter("pingurl"));
+		String command = request.getParameter("command");
+		String op = "";
+		if(command != null) {
+		Process process;
+		switch(command) {
+			case "ping": process = Runtime.getRuntime().exec("ping "+request.getParameter("pingurl")); break;
+			case "tracert": process = Runtime.getRuntime().exec("tracert "+request.getParameter("tracerturl")); break;
+			case "nslookup": process = Runtime.getRuntime().exec("nslookup "+request.getParameter("nslookupurl")); break;
+			case "pathping": process = Runtime.getRuntime().exec("pathping "+request.getParameter("pathpingurl")); break;
+			case "ipconfig": process = Runtime.getRuntime().exec("ipconfig"); break;
+			case "getmac": process = Runtime.getRuntime().exec("getmac"); break;
+			case "netstat": process = Runtime.getRuntime().exec("netstat"); break;
+			default: process = null;
+		}
 		
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 	    String line = null;
-	    String op = "";
+	    
 	    while ((line = reader.readLine()) != null) {
-	    	op = op.concat(line);
-	    	op = op.concat("\n");
+	    	op = op.concat(URLEncoder.encode(line,"UTF-8"));
+	    	op = op.concat("%0A");
 	        System.out.println(line+op);
 	    }
-	    op = op.replace("%","percent");
+	    
+	   // op = op.replace("%","percent");
 
+		}
         System.out.println("op: "+op);
 		response.sendRedirect("index.jsp?done="+op);
+		
 	}
 
 }
